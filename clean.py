e0fee332aeb24b3d974d43f6c33773da48c577ba
josekey <jose.key@yale.edu>
2021-08-10 14:36:45 -0600

    second commit

--- clean.py
+++ clean.py
@@ -12,6 +12,190 @@ import simplejson
 def main():
     make_activity_json('Rbm Building Services')
 
+#### create engagement payloads for each type
+# collecting metadata for Hubspot APi call
+def samson(data):
+    # loop through companies
+    for row in data:
+        # pull activity data
+        activities = row['activities']
+
+        # loop through activities
+        for act in activities:
+            # general activity information 
+            timestamp = ISO_to_UNIX(act['activity_at'])
+            user = act["user_name"]
+
+            # if email
+            if act['_type'] == 'Email':
+                type = 'EMAIL'
+
+                env = act['envelope']
+                f = {'email':env['from'][0]['email'],
+                        'firstName':env['from'][0]['name'].split()[0],
+                        'lastName':env['from'][0]['name'].split()[-1]}
+                to = f"{env['sender'][0]['name']} <{env['sender'][0]['email']}>"
+                cc = act['cc']
+                bcc = act['bcc']
+                html = act['body_html']
+                text = act['body_text']
+                subject = env['subject']
+
+                email_metadata(f, to, cc, bcc, subject, html, text)
+            
+            # if meeting
+            elif act['_type'] == 'Meeting':
+                type = 'MEETING'
+
+                body = "",
+                startTime = ISO_to_UNIX(act['starts_at']), ### unix time
+                endTime = ISO_to_UNIX(act['ends_at']),
+                title = act['title'],
+                internalMeetingNotes = act['note']
+
+                meeting_metadata(body, startTime, endTime, title, internalMeetingNotes)
+
+            # if note
+            elif act['_type'] == 'Note':
+                type = 'NOTE'
+
+                {'body':act['note']}
+
+            # if call
+            elif act['_type'] == 'Call':
+                type = "CALL"
+
+                # metadata 
+                if act['direction'] == 'outbound':
+                    toNumber = act['phone']
+                    fromNumber = act["local_phone"]
+                else:
+                    fromNumber = act['phone']
+                    toNumber = act["local_phone"]
+                status = "COMPLETED"
+                durationMilliseconds = act['duration']*1000
+                if act["has_recording"]:
+                    recordingUrl = act['recording_url']
+                else: 
+                    recordingUrl = ""
+
+                body = act["note"]
+
+                call_metadata(toNumber, fromNumber, status, durationMilliseconds, recordingUrl, body)
+
+            # if task
+            elif act["_type"] == "TaskCompleted":
+                body = act['task_text']
+                subject = "Imported Task"
+                status = "COMPLETED",
+                forObjectType = "COMPANY"
+
+
+
+
+### note metadata type already created
+
+### email    
+def email_metadata(f, to, cc, bcc, subject, html, text):
+    return {
+        "from": f,
+        "to": [to],
+        "cc": cc,
+        "bcc": bcc,
+        "subject": subject, 
+        "html": html, #"body_html"
+        "text": text #'body_text'
+    }
+
+### meeting
+def meeting_metadata(body, startTime, endTime, title, internalMeetingNotes):
+    return {
+        "body": body,
+        "startTime": startTime, ### unix time
+        "endTime": endTime,
+        "title": title,
+        "internalMeetingNotes" : internalMeetingNotes
+    }
+
+### call
+def call_metadata(toNumber, fromNumber, status, durationMilliseconds, recordingURL, body):
+    return {
+        "toNumber" : toNumber,
+        "fromNumber" : fromNumber,
+        "status" : status,
+        "durationMilliseconds" : durationMilliseconds,
+        "recordingUrl" : recordingURL,
+        "body" : body
+    }
+
+### tasks
+def tasks_metadata():
+    return {
+        "body": "This is the body of the task.",
+        "subject": "Task title",
+        "status": "NOT_STARTED",
+        "forObjectType": "CONTACT"
+    }
+        
+### creating singular engagement dictionary payload  
+def create_engagement(ownerID, type, time, metadata):
+    {
+        "engagement": {
+            "active": 'true', #--
+            "ownerId": 1, # Map User to ID // if user is no longer with us --> use Jose ID
+            "type": type, #--
+            "timestamp": time #--
+        },
+        "associations": { #### todo
+            "contactIds": [11877974],
+            "companyIds": [ ],
+            "dealIds": [ ],
+            "ownerIds": [ ]
+        },
+        ### optional
+        # "attachments": [
+        #     {
+        #         "id": 4241968539
+        #     }
+        # ],
+        "metadata": metadata
+    }
+
+
+def load_close():
+    #load close data
+    contacts = pd.read_csv('/Users/jose/Documents/program_projects/CRM_clean/Close/Angle Health contacts 2021-08-05 18-30.csv')
+    leads = pd.read_csv('/Users/jose/Documents/program_projects/CRM_clean/Close/Angle Health leads 2021-08-05 18-30.csv')
+    opportunities = pd.read_csv('/Users/jose/Documents/program_projects/CRM_clean/Close/Angle Health opportunities 2021-08-05 18-30.csv')
+    
+    return contacts, leads, opportunities
+
+
+### save beaufified json with activity for one company
+def make_activity_json(company):
+    f = open('/Users/jose/Documents/program_projects/crm-migration/Close/Angle Health leads 2021-08-09 21-39.json')
+    data = json.load(f)
+
+    df = pd.DataFrame(data=data)
+
+    # temp = df.loc[df['name'] == 'BAER WELDING', 'activities'].to_dict()
+    temp = df.loc[df['name'] == company, 'activities'].to_dict()
+
+    # save beautified activities for specific company 
+    savefile = open(f'/Users/jose/Documents/program_projects/crm-migration/{company}.json','w')
+    savefile.write(simplejson.dumps(simplejson.loads(json.dumps(temp)), indent=4, sort_keys=True))
+    savefile.close()
+
+
+if __name__ == "__main__":
+    main()
+
+############ NOTES ############
+
+# mappings for users, contact, companies, and deals to HubSpot ID
+
+################# JUNK ############
+
     # populated = []
     # for i in range(0, len(data)):
     #     if data[i]['tasks'] or data[i]['activities']:
@@ -51,138 +235,3 @@ def main():
     ## ad content to 'url' in leads
     ## associate leads(companies) and deals based on company name
 
-#### create engagement payloads for each type
-# collecting metadata for Hubspot APi call
-def samson(data):
-    # loop through companies
-    for row in data:
-        # pull activity data
-        activities = row['activities']
-
-        # loop through activities
-        for act in activities:
-            # if email
-            if act['_type'] == 'Email':
-                env = act['envelope']
-                f = {'email':env['from'][0]['email'],
-                        'firstName':env['from'][0]['name'].split()[0],
-                        'lastName':env['from'][0]['name'].split()[-1]}
-                to = f"{env['sender'][0]['name']} <{env['sender'][0]['email']}>"
-                cc = act['cc']
-                bcc = act['bcc']
-                html = act['body_html']
-                text = act['body_text']
-                subject = env['subject']
-
-                email_metadata(f, to, cc, bcc, subject, html, text)
-            
-            #if meeting
-            elif act['_type'] == 'Meeting':
-                body = "",
-                startTime = ISO_to_UNIX(act['starts_at']), ### unix time
-                endTime = ISO_to_UNIX(act['ends_at']),
-                title = act['title'],
-                internalMeetingNotes = act['note']
-
-                meeting_metadata(body, startTime, endTime, title, internalMeetingNotes)
-
-            elif act['_type'] == 'Note':
-                {'body':act['note']}
-
-            elif act['_type'] == 'Call':
-                "toNumber" : "5618769964",
-                "fromNumber" : "(857) 829-5489",
-                "status" : "COMPLETED",
-                "durationMilliseconds" : 38000,
-                "recordingUrl" : act['recording_url'],
-                "body" : ""
-
-
-
-### note metadata type already created
-
-### email    
-def email_metadata(f, to, cc, bcc, subject, html, text):
-    return {
-        "from": f,
-        "to": [to],
-        "cc": cc,
-        "bcc": bcc,
-        "subject": subject, 
-        "html": html, #"body_html"
-        "text": text #'body_text'
-    }
-
-### meeting
-def meeting_metadata(body, startTime, endTime, title, internalMeetingNotes):
-    return {
-        "body": body,
-        "startTime": startTime, ### unix time
-        "endTime": endTime,
-        "title": title,
-        "internalMeetingNotes" : internalMeetingNotes
-    }
-
-### call
-def call_metadata():
-    return {
-        "toNumber" : "5618769964",
-        "fromNumber" : "(857) 829-5489",
-        "status" : "COMPLETED",
-        "durationMilliseconds" : 38000,
-        "recordingUrl" : "https://api.twilio.com/2010-04-01/Accounts/AC890b8e6fbe0d989bb9158e26046a8dde/Recordings/RE3079ac919116b2d22",
-        "body" : ""
-    }
-        
-### creating singular engagement dictionary payload  
-def create_engagement(ownerID, type, time, metadata):
-    {
-        "engagement": {
-            "active": 'true',
-            "ownerId": 1,
-            "type": type,
-            "timestamp": time # unix
-        },
-        "associations": {
-            "contactIds": [11877974],
-            "companyIds": [ ],
-            "dealIds": [ ],
-            "ownerIds": [ ]
-        },
-        ### optional
-        "attachments": [
-            {
-                "id": 4241968539
-            }
-        ],
-        "metadata": metadata
-    }
-
-
-def load_close():
-    #load close data
-    contacts = pd.read_csv('/Users/jose/Documents/program_projects/CRM_clean/Close/Angle Health contacts 2021-08-05 18-30.csv')
-    leads = pd.read_csv('/Users/jose/Documents/program_projects/CRM_clean/Close/Angle Health leads 2021-08-05 18-30.csv')
-    opportunities = pd.read_csv('/Users/jose/Documents/program_projects/CRM_clean/Close/Angle Health opportunities 2021-08-05 18-30.csv')
-    
-    return contacts, leads, opportunities
-
-
-### save beaufified json with activity for one company
-def make_activity_json(company):
-    f = open('/Users/jose/Documents/program_projects/crm-migration/Close/Angle Health leads 2021-08-09 21-39.json')
-    data = json.load(f)
-
-    df = pd.DataFrame(data=data)
-
-    # temp = df.loc[df['name'] == 'BAER WELDING', 'activities'].to_dict()
-    temp = df.loc[df['name'] == company, 'activities'].to_dict()
-
-    # save beautified activities for specific company 
-    savefile = open(f'/Users/jose/Documents/program_projects/crm-migration/{company}.json','w')
-    savefile.write(simplejson.dumps(simplejson.loads(json.dumps(temp)), indent=4, sort_keys=True))
-    savefile.close()
-
-
-if __name__ == "__main__":
-    main()
